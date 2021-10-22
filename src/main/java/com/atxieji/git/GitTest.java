fb0fa62725d83d14a6d2eb967cdc0cf9628a8fc4
lmh <xieji366@gmail.com>
2021-10-22 14:45:10 +0800

    first commit

---
+++ src/main/java/com/atxieji/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atxieji.git;
+
+/**
+ * @author lmhstart
+ * @create 2021/10/22-14:42
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,git!");
+    }
+
+}
